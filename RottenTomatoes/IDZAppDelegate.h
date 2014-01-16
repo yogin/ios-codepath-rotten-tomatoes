30ca9e94454445e4cda62d8295bf72c8414873d5
Anthony Powles <github@idreamz.net>
2014-01-16 14:27:21 -0800

    initial commit

---
+++ RottenTomatoes/IDZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IDZAppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Anthony Powles on 1/16/14.
+//  Copyright (c) 2014 Anthony Powles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RottenTomatoes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RottenTomatoes
+//
+//  Created by Anthony Powles on 1/16/14.
+//  Copyright (c) 2014 Anthony Powles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IDZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([IDZAppDelegate class]));
+	}
+}
